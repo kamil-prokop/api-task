31b0244b276de604e99569c42a96833f44885140
Kamil Prokop <zara.pug.yt@gmail.com>
2023-02-17 00:02:57 +0100

    CSV file created

--- api-task.py
+++ api-task.py
@@ -1,6 +1,10 @@
-import requests
+import requests, csv
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
 
-print(data)
+with open('currencies.csv', 'w', newline='') as f:
+    writer = csv.writer(f, delimiter=";")
+    writer.writerow(data[0]['rates'][0].keys()) #keys
+    for i in range(0, len(data[0]['rates'])): #no of elements
+        writer.writerow(data[0]['rates'][i].values()) #values
